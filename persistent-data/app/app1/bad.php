d59a363d9189f60041a7b37a22511b8b568d05cc
areaeuro <john@area-europa.net>
2019-05-06 01:03:18 +0200

    Complete rewrite and adaption to my docker file structure

---
+++ persistent-data/app/app1/bad.php
@@ -0,0 +1,6 @@
+<?php
+ 
+echo '<h1>This is bad file which will create entry in log file</h1>';
+print("\n\n<br><br>log_errors: ".ini_get('log_errors'));
+print("\n\n<br><br>error_log: ".ini_get('error_log'));
+echo ''
